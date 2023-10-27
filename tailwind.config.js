d044f2477cd8ed3a4b7312235eef676ffa4c5e4a
AshleeAWebb <ashlee.webb.18@gmail.com>
2023-10-27 10:58:18 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'C:\\Users\\ashlee.webb\\Learning\\tailwind_practice\\index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
